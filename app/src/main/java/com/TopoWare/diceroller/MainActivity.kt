efd2ac5cd2b7f26812a901af2c3f125a0e107b8a
Diego Ginko <diego.ginko@maxirest.com>
2020-06-22 14:45:25 -0300

    Subo repo

---
+++ app/src/main/java/com/TopoWare/diceroller/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.TopoWare.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val btnDice: Button = findViewById(R.id.btnDice)
+        diceImage = findViewById(R.id.dice_image)
+
+        btnDice.setOnClickListener{
+            //Toast.makeText(this,"Se toco el boton",Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
